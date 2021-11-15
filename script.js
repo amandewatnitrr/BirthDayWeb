7f02dabe002839425a31757fb1707e64e1403869
Alim Khan <workingalim@gmail.com>
2021-11-15 20:09:45 +0530

    third commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var textWrapper = document.querySelector('.ml10 .letters');
+var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml10 .letter',
-    rotateY: [-90, 0],
-    duration: 1300,
-    delay: (el, i) => 45 * i
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
   }).add({
-    targets: '.ml10',
+    targets: '.ml2',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
